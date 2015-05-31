f95c59b8f5bdb7f7802dd88f840d359a76874929
Victor Powell <vicapow@gmail.com>
2015-05-30 23:40:32 -0700

    the birth of a test repo.

---
+++ main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+mapboxgl.accessToken = getAccessToken();
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  zoom: 12.5,
+  center: [38.888, -77.01866],
+  style: 'https://www.mapbox.com/mapbox-gl-styles/styles/bright-v7.json',
+  hash: true
+});
+
+map.on('style.load', function() {
+  map.addSource('geojson', {
+    "type": "geojson",
+    "data": "route.json"
+  });
+
+  map.addLayer({
+    "id": "route",
+    "type": "line",
+    "source": "geojson",
+    "paint": {
+      "line-color": "#EC8D8D",
+      "line-width": "@motorway_width"
+    }
+  }, 'country_label_1');
+});
+
+function getAccessToken() {
+  var match = location.search.match(/access_token=([^&\/]*)/);
+  var accessToken = match && match[1];
+
+  if (accessToken) {
+    localStorage.accessToken = accessToken;
+  } else {
+    accessToken = localStorage.accessToken;
+  }
+
+  return accessToken;
+}
